300179b9cd2fdd944ab32b74533ef486f2c2a685
Leandro Reis <lleandrofr@gmail.com>
2021-11-03 15:53:02 -0300

    fix: change JWT Secret env variable name

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -2,7 +2,11 @@ const { CREATED, INTERNAL_SERVER_ERROR } = require('http-status');
 
 const createTask = async (req, res) => {
   try {
-    return res.status(CREATED).json();
+    const { userId } = req;
+
+    console.log(userId);
+
+    return res.status(CREATED).json({ message: 'login ok' });
   } catch (error) {
     console.log(error.message);
     return res.status(INTERNAL_SERVER_ERROR).send('Something went wrong :(');
--- backend/src/routes/taskRoute.js
+++ backend/src/routes/taskRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const validateJWT = require('../auth/validateJWT');
 const { createTask } = require('../controllers/taskController');
 
 const router = express.Router();
 
-router.route('/').post(createTask);
+router.route('/').post(validateJWT, createTask);
 
 module.exports = router;
