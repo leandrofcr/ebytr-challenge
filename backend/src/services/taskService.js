b8b589ffe522a2e839d534569a69aafb644a4417
Leandro Reis <lleandrofr@gmail.com>
2021-11-03 17:07:04 -0300

    feat: creates a endpoint POST do create a new task

--- backend/src/services/taskService.js
+++ backend/src/services/taskService.js
@@ -1,10 +1,21 @@
 const { checkTaskEntries } = require('../validations/taskValidation');
+const taskModel = require('../model/taskModel');
 
 const createTask = async (userId, task, status) => {
   const taskEntries = checkTaskEntries(task, status);
   if (taskEntries.message) return taskEntries;
 
-  return { userId, task, status };
+  const date = new Date();
+  const createdAt = date.toLocaleString();
+
+  const createdTask = await taskModel.createTask({
+    task,
+    status,
+    userId,
+    createdAt
+  });
+
+  return createdTask;
 };
 module.exports = {
   createTask
